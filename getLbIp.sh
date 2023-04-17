18030063f25f412e43a37cca740c5bcf70f67969
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2023-04-17 16:18:28 -0500

    Update getLbIp.sh
    Remove pending

--- getLbIp.sh
+++ getLbIp.sh
@@ -4,7 +4,7 @@ lbIp=""
 while [[ ${#lbIp} -le 5 ]]
 do
   echo "$0: Checking if Loadbancer IP is assigned ... "
-  lbIp=$(kubectl get svc  | grep -i LoadBalancer | awk '{ print $4 }' )
+  lbIp=$(kubectl get svc  | grep -i LoadBalancer | grep -v pending | awk '{ print $4 }' )
   echo "$0: Ingress Loadbalancer IP is :" $lbIp, "lbIp length is : " ${#lbIp}
   sleep 10
 done
